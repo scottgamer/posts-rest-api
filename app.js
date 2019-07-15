760e34005904f4fac3ec8a26f362fe4d26abbc28
scottgamer <richard.munoz@epn.edu.ec>
2019-07-14 23:16:15 -0500

    Get post route added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
+
 
 const feedRoutes = require('./routes/feed');
 
@@ -8,6 +10,7 @@ const app = express();
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -17,6 +20,15 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+// Error handling middleware
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json(message);
+});
+
 mongoose.connect('mongodb://localhost:27017/posts', { useNewUrlParser: true }).then(result => {
   app.listen(8080);
 }).catch(err => console.log(err));
--- controllers/feed.js
+++ controllers/feed.js
@@ -3,29 +3,24 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: '1',
-        title: 'First Post',
-        content: 'This is the first post!',
-        imageUrl: 'images/duck.jpg',
-        creator: {
-          name: 'Richard',
-        },
-        createdAt: new Date()
-      }]
-  });
+  Post.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422)
-      .json({
-        message: 'Validation failed',
-        errors: errors
-      });
+    const error = new Error('Validation failed');
+    error.statusCode = 422;
+    throw error;
   }
   const title = req.body.title;
   const content = req.body.content;
@@ -46,7 +41,28 @@ exports.createPost = (req, res, next) => {
       });
     })
     .catch(err => {
-      console.log(err);
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
     });
-
 };
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.id;
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Post not found!');
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+}
